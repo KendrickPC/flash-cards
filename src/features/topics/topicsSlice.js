85374098f58948689d41c44f4fae631f1925dfd6
kendrickPC <kennethpchang@gmail.com>
2021-08-06 11:59:17 -0700

    first commit

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+  name: 'topics',
+  initialState: {
+    topics: {
+      // Will eventually hold all topics keyed by id
+    }
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      // topics keyed by id
+      state.topics[action.payload.id] = {
+        id: action.payload.id,
+        name: action.payload.name,
+        icon: action.payload.icon,
+        quizIds: [],
+      }
+    }
+  }
+})
+
+export const selectTopics = (state) => state.topics.topics;
+export const topicsReducer = topicsSlice.reducer;
